db4a422afa5a5a6e6bee1702862ca9b610a317ac
Sunil Kumar <kalathilsunil@yahoo.com>
2015-11-04 16:40:40 +0530

    Modified to include date time

--- server/onStart.js
+++ server/onStart.js
@@ -1,9 +1,10 @@
 Meteor.startup(function () {
   // code to run on server at startup
 	for(i=0;i<20;i++){
-		user = "user" + i;
-		message = "dkdkadadas"
-		chatMessages.insert({user:user,message:message});
+		var user = "user" + i;
+		var message = "dkdkadadas";
+		var now = new Date();
+		chatMessages.insert({user:user,message:message,date:now});
 	}
 	
 	
