d27161723467868c01f5c9c906a94d9c453665d9
Sunil Kumar <kalathilsunil@yahoo.com>
2015-11-04 16:39:58 +0530

    Message time
    Modified to capture message time. Sort messages in the order of
    timestamp. Included moment() js functions to show when a chat message
    was recorded.

--- client/scripts/chatWindow.js
+++ client/scripts/chatWindow.js
@@ -1,7 +1,20 @@
 Template.chatWindow.helpers({
 	messages:function(){
 		
-		return chatMessages.find({});
+		return chatMessages.find({},{sort:{"date": 1}});
+	},
+	
+	messagetime: function(){
+			
+		// A common way of displaying time is handled by moment#fromNow. This is sometimes called timeago or relative time.
+		//
+		// moment([2007, 0, 29]).fromNow(); // 4 years ago
+		// If you pass true, you can get the value without the suffix.
+		//
+		// moment([2007, 0, 29]).fromNow();     // 4 years ago
+		// moment([2007, 0, 29]).fromNow(true); // 4 years
+		
+		return moment(this.date).fromNow();
 	},
 	
 	avatarText: function(){
@@ -13,8 +26,9 @@ Template.chatWindow.helpers({
 Template.chatWindow.events({
 	'submit form': function(e,template){
 		e.preventDefault();
-		message = template.find('#mymessage').value;
-		chatMessages.insert({user:Meteor.user().username,message:message});
+		var message = template.find('#mymessage').value;
+		var now = new Date();
+		chatMessages.insert({user:Meteor.user().username,message:message,date:now});
 		var wtf    = $('#chats');
 		var height = wtf[0].scrollHeight;
 		wtf.scrollTop(height);
