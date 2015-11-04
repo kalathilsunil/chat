159e86fa1365ea05f7c9b261c56205ad629c451d
Sunil Kumar <kalathilsunil@yahoo.com>
2015-11-04 18:14:28 +0530

    Scroll to the bottom of chatwindow when user logs in

--- client/scripts/chatWindow.js
+++ client/scripts/chatWindow.js
@@ -1,6 +1,7 @@
 Template.chatWindow.helpers({
 	messages:function(){
-		
+		//Return all the chatmessages from the collection, sorted by date so that latest message shows
+		//up in the bottom ot chatwindow.
 		return chatMessages.find({},{sort:{"date": 1}});
 	},
 	
@@ -18,6 +19,7 @@ Template.chatWindow.helpers({
 	},
 	
 	avatarText: function(){
+		// Return the first character of username in uppercase to show as avatar text
 		return(this.user.charAt(0).toUpperCase());
 	}
 });
@@ -25,12 +27,26 @@ Template.chatWindow.helpers({
 
 Template.chatWindow.events({
 	'submit form': function(e,template){
+		//Prevent the default form actions.
 		e.preventDefault();
 		var message = template.find('#mymessage').value;
 		var now = new Date();
+		
+		//Insert to chatmessages collection the active username, message and current data time stamp.
 		chatMessages.insert({user:Meteor.user().username,message:message,date:now});
+		
+		//Scroll to the bottom of chatwindow to show the posted chat message. 
 		var wtf    = $('#chats');
 		var height = wtf[0].scrollHeight;
 		wtf.scrollTop(height);
 	}
+});
+
+Template.chatWindow.onRendered(function(){
+	//Scroll to the bottom of chatwindow to show the latest post.
+	// This method is called once when an instance of Template.chatWindow is rendered into DOM nodes and
+	// put into the document for the first time.
+	var wtf    = $('#chats');
+	var height = wtf[0].scrollHeight;
+	wtf.scrollTop(height);
 });
